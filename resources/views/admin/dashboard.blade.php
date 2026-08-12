8d4128ec1d7469f860c6dba22a14b5897c2f908f
Znedu <pdenzashley@gmail.com>
2026-08-13 04:51:00 +0800

    Test GitHub Hostinger sync

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,6 +11,7 @@
     </div>
 
     {{-- Stats Grid --}}
+    {{-- Stats Grid --}}
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         <x-stat-card
             title="Total Bookings"
