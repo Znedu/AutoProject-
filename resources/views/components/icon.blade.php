2337261ff6c934fdf7896516a55090c8abf4c325
Znedu <pdenzashley@gmail.com>
2026-08-18 01:53:55 +0800

    feat: add reusable blade icon component with dynamic svg support

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -250,6 +250,21 @@
         </svg>
         @break
 
+    @case('key')
+    @case('key-round')
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="{{ $class }}">
+            <path d="M2 18v3c0 .6.4 1 1 1h4v-3h3v-3h2l1.4-1.4a6.5 6.5 0 1 0-4-4Z" />
+            <circle cx="16.5" cy="7.5" r=".5" fill="currentColor" />
+        </svg>
+        @break
+
+    @case('lock')
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="{{ $class }}">
+            <rect width="18" height="11" x="3" y="11" rx="2" ry="2" />
+            <path d="M7 11V7a5 5 0 0 1 10 0v4" />
+        </svg>
+        @break
+
     @default
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="{{ $class }}">
             <circle cx="12" cy="12" r="10" />
